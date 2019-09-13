7751ede1f9685cc1b55a4409329b3449f1fc5469
Robin Dittrich <462270@student.saxion.nl>
2019-09-13 16:54:15 +0200

    [/] changed main code for testing at home

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,15 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            HueBridge bridge = new HueBridge("localhost", "66986704230b2e75868416979af78fe"); //pc emulator hue
-//            HueBridge bridge = new HueBridge("192.168.1.102", "8f36bb73f410a65f044469ea5b645dca", 5); //home diyhue
+//            HueBridge bridge = new HueBridge("localhost", "66986704230b2e75868416979af78fe"); //pc emulator hue
+            HueBridge bridge = new HueBridge("192.168.1.102", "8f36bb73f410a65f044469ea5b645dca", 5); //home diyhue
 //            HueBridge bridge = new HueBridge("192.168.1.50", "4lHzCIy-2gZKkUKD75tHXWhvWvDEOCxpJP9YILGF", 0); //syntaxis hue
 
             System.out.println(bridge.getLights());
             //init loop
             for (HueLight light : bridge.getLights()) {
                 light.setPower(true);
-                light.setBri(254);
                 light.setRGB(0, 255, 0);
-                light.setTransitionTime(0);
             }
 
 
